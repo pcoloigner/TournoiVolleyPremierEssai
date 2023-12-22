44366e11e0605217254b01ed4bf4fc599ee710d8
pcoloigner <philippe.coloigner@gmail.com>
2023-12-22 11:03:25 +0100

    Creation d'une application tournoivolley-database dans le repertoire 'infrastructure'

---
+++ infrastructure/tournoivolley-database/src/main/java/org/pcomeziantou/tournoivolleydatabase/TournoivolleyDatabaseApplication.java
@@ -0,0 +1,13 @@
+package org.pcomeziantou.tournoivolleydatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TournoivolleyDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TournoivolleyDatabaseApplication.class, args);
+	}
+
+}
