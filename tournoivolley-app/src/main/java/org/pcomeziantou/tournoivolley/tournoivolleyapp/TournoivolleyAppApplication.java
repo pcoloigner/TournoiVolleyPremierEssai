592cb42303a04caf182b6a0f825468a64a8757fc
pcoloigner <philippe.coloigner@gmail.com>
2023-12-30 12:46:57 +0100

    Réorganisation, selon https://github.com/youenchene/featurebrawl

---
+++ tournoivolley-app/src/main/java/org/pcomeziantou/tournoivolley/tournoivolleyapp/TournoivolleyAppApplication.java
@@ -0,0 +1,13 @@
+package org.pcomeziantou.tournoivolley.tournoivolleyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TournoivolleyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TournoivolleyAppApplication.class, args);
+	}
+
+}
---
+++ tournoivolley-app/src/test/java/org/pcomeziantou/tournoivolley/tournoivolleyapp/TournoivolleyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pcomeziantou.tournoivolley.tournoivolleyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TournoivolleyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
